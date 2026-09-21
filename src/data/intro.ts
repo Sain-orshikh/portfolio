2b36ead3cce98c841371acd939bc1831d71c28bd
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2026-09-21 22:55:35 +0800

    Update homepage introduction text

--- src/data/intro.ts
+++ src/data/intro.ts
@@ -1,8 +1,8 @@
 export const introText = {
-  title: "I'm a high school senior at Mongol Aspiration International School, passionate about exploring all fields of computer science.",
+  title: "I'm a freshman at Korea Advanced Institute of Science and Technology (KAIST), passionate about exploring all fields of computer science.",
   paragraphs: [
     "At the moment, I'm taking a break from web development to focus on my studies and other fields of computer science. In the past, I've had the opportunity to work as a web developer across a variety of settings, from mid-sized corporations and start-ups to school organizations.",
-    "In my spare time, I'm usually enjoying my last year of high school with friends or working on new and interesting project ideas, mostly on weekends."
+    "In my spare time, I'm usually enjoying my first year of university or working on new and interesting project ideas, mostly on weekends."
   ],
-  learning: "Currently, I'm working on finishing my school's official website before my leave and learning the basics of AI through a small project, like a Flappy Bird AI"
+  learning: "Currently, I'm working on a computer vision project for sign language translation."
 };
